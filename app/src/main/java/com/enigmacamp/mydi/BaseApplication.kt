45c2fdb85dd1a2305c13cfe5ea48fa2a060521d4
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-08-20 06:18:46 +0700

    private set on initialization component

--- app/src/main/java/com/enigmacamp/mydi/BaseApplication.kt
+++ app/src/main/java/com/enigmacamp/mydi/BaseApplication.kt
@@ -11,9 +11,13 @@ import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class BaseApplication : DaggerApplication() {
-    private lateinit var salesComponent: SalesComponent
-    private lateinit var vehicleComponent: VehicleComponent
-    private lateinit var dataComponent: DataComponent
+    lateinit var salesComponent: SalesComponent
+        private set
+    lateinit var vehicleComponent: VehicleComponent
+        private set
+    lateinit var dataComponent: DataComponent
+        private set
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         dataComponent = DaggerDataComponent.create()
         salesComponent = DaggerSalesComponent.create()
